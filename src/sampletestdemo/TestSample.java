3b93f2f7c1e90980c5adfc2fb4213176ef3b3fef
Zuci Systems <Zuci Systems@192.168.1.6>
2016-10-21 17:09:04 +0530

    Updated Code.

--- src/sampletestdemo/TestSample.java
+++ src/sampletestdemo/TestSample.java
@@ -11,27 +11,34 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class TestSample {
 
 		@SuppressWarnings("rawtypes")
-		private static AndroidDriver driver;
+		public static AndroidDriver driver;
 		@SuppressWarnings("rawtypes")
 		public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
-			File appDir = new File( "F:\\Project Softwares");
-			File app = new File(appDir, "com.android.chrome.apk");
+			try{
+			//File appDir = new File( "F:\\Project Softwares");
+			File app = new File("F:\\Project Softwares\\com.android.chrome-45.0.2454.84-245408400-minAPI16.apk");
 
 			DesiredCapabilities capabilities = new DesiredCapabilities();
 			capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
-			capabilities.setCapability("deviceName", "emulator-5554");
-			capabilities.setCapability("platformVersion", "5.1.1");
+			capabilities.setCapability("deviceName", "ZuciTest");
+			capabilities.setCapability("platformVersion", "6.0");
 			capabilities.setCapability("platformName", "Android");
 			capabilities.setCapability("app", app.getAbsolutePath());
 			capabilities.setCapability("appPackage", "com.android.chrome");
-			capabilities.setCapability("appActivity", "com.android.chrome.HomeActivity");
+			capabilities.setCapability("appActivity", "com.android.chrome.MainActivity");
 
 			driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 			driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+			driver.get("http://www.google.com");
 			Thread.sleep(10000);
-			driver.quit();
+			
 
+			} catch(Exception e){
+				e.printStackTrace();
+			} finally{
+				driver.quit();
+			}
 	}
 
 }
--- src/sampletestdemo/sample.java
+++ src/sampletestdemo/sample.java
@@ -19,23 +19,24 @@ public class sample {
 		// set up appium and tell from where it can install the apk file from
 		// computer to device
 		File appDir = new File("F:\\Project Softwares");
-		File app = new File(appDir, "com.android.chrome.apk");
+		File app = new File(appDir, "org.mozilla.firefox.apk");
 		// Very important properties you need for Appium to work, change as per
 		// SDK and device name
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
-		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability(CapabilityType.PLATFORM, "Android");
 		capabilities.setCapability(CapabilityType.VERSION, "5.1.1");
-		capabilities.setCapability("deviceName", "emaulator-5554");
+		capabilities.setCapability("deviceName", "ZuciTest");
 		capabilities.setCapability("autoLaunch",true);
 		capabilities.setCapability("automationName", "Appium");
 		capabilities.setCapability("app", app.getAbsolutePath());
 		
-		capabilities.setCapability("appPackage", "com.android.chrome.apk");
-		capabilities.setCapability("appActivity", "com.android.chrome.Main");
+		capabilities.setCapability("appPackage", "org.mozilla.firefox");
+		capabilities.setCapability("appActivity", "org.mozilla.gecko.BrowserApp");
 		// The URL where the hub will start
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
 		driver.close();
 	}
 	}
