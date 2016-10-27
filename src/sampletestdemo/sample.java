2eca6c7eb85f8b57dcda65bb996f5337a51f9b1b
Zuci Systems <Zuci Systems@192.168.1.6>
2016-10-27 14:13:00 +0530

    Updated Code.

--- src/sampletestdemo/sample.java
+++ src/sampletestdemo/sample.java
@@ -1,6 +1,5 @@
 package sampletestdemo;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +14,7 @@ import net.lightbody.bmp.core.har.Har;
 
 public class sample {
 
-	@SuppressWarnings({ "rawtypes" })
+	@SuppressWarnings({ "rawtypes", "static-access" })
 	public static void main(String [] args) throws Exception {
 		
 		BrowserMobProxyServer server = new BrowserMobProxyServer();
@@ -24,33 +23,31 @@ public class sample {
 		
 		try{
 			server.start();
-
+	
+			/*			Get the Selenium Proxy Object			*/
 			Proxy proxy = ClientUtil.createSeleniumProxy(server);
 		
-		capabilities = new DesiredCapabilities();
-		capabilities.setCapability(CapabilityType.BROWSER_NAME, "Browser");
-		capabilities.setCapability("automationName", "Appium");
-		capabilities.setCapability(CapabilityType.PLATFORM, "Android");
-		capabilities.setCapability(CapabilityType.PROXY, proxy);
-		capabilities.setCapability(CapabilityType.VERSION, "4.4.1");
-		capabilities.setCapability("deviceName", "Zu");
-		capabilities.setCapability("appPackage", "com.android.browser");
-		capabilities.setCapability("appActivity", "com.android.browser.BrowserActivity");
-		capabilities.setCapability("noReset", false);
+			capabilities = new DesiredCapabilities().android();
+			capabilities.setCapability(CapabilityType.BROWSER_NAME, "Browser");
+			capabilities.setCapability("automationName", "Appium");
+			capabilities.setCapability(CapabilityType.PLATFORM, "Android");
+			capabilities.setCapability(CapabilityType.PROXY, proxy);
+			capabilities.setCapability(CapabilityType.VERSION, "4.4.1");
+			capabilities.setCapability("deviceName", "Zu");
+			capabilities.setCapability("appPackage", "com.android.browser");
+			capabilities.setCapability("appActivity", "com.android.browser.BrowserActivity");
+			capabilities.setCapability("noReset", false);
 		
-		server.newHar("facebook.com");
-
+			
+		
+		server.newHar("http://www.facebook.com");
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.get("http://www.facebook.com");
 		try{
-			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 			Har har = server.getHar();
-			
-			File file = new File("Sample.har");
-			FileOutputStream fos = new FileOutputStream(file);
+			FileOutputStream fos = new FileOutputStream("Sample.har");
 			har.writeTo(fos);
-		
 		} catch (Exception e){
 			e.printStackTrace();
 		} 
