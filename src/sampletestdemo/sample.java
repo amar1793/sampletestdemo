4dff80e05821362e8ec348975cf60fbafaf54ffa
Zuci Systems <Zuci Systems@192.168.1.6>
2016-11-02 17:29:14 +0530

    Updated Code.

--- src/sampletestdemo/sample.java
+++ src/sampletestdemo/sample.java
@@ -2,7 +2,6 @@ package sampletestdemo;
 
 import java.io.FileOutputStream;
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,7 +13,7 @@ import net.lightbody.bmp.core.har.Har;
 
 public class sample {
 
-	@SuppressWarnings({ "rawtypes", "static-access" })
+	@SuppressWarnings({ "rawtypes" })
 	public static void main(String [] args) throws Exception {
 		
 		BrowserMobProxyServer server = new BrowserMobProxyServer();
@@ -37,28 +36,29 @@ public class sample {
 			capabilities.setCapability("appActivity", "com.android.browser.BrowserActivity");
 			capabilities.setCapability("noReset", false); */
 			
-			capabilities = new DesiredCapabilities().android();
+			capabilities = new DesiredCapabilities();
 			capabilities.setCapability(CapabilityType.PROXY, proxy);
 			capabilities.setCapability("automationName", "Appium");
 			capabilities.setCapability("platformName", "Android");
-			capabilities.setCapability("platformVersion", "5.1.1");
-			capabilities.setCapability("deviceName", "emulator-5558");
-			capabilities.setCapability("browserName", "");
-			capabilities.setCapability("newCommandTimeout", 60);
-			capabilities.setCapability("appPackage", "org.mozilla.fennec");
-			capabilities.setCapability("appActivity", "org.mozilla.gecko.BrowserApp");
+			capabilities.setCapability("platformVersion", "4.4.2");
+			capabilities.setCapability("deviceName", "Zu");
+			capabilities.setCapability("browserName", "Browser");
+			capabilities.setCapability("newCommandTimeout", 120);
+			capabilities.setCapability("appPackage", "com.android.browser");
+			capabilities.setCapability("appActivity", "com.android.browser.BrowserActivity");
 		
 			//driver = new RemoteWebDriver(capabilities);
 			//server.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
 			
 		server.newHar("http://www.rediff.com");
+		//Thread.sleep(5000);
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-		driver.manage().timeouts().implicitlyWait(60000, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(60000, TimeUnit.SECONDS);
 		driver.get("http://www.rediff.com");
 		try{
-			driver.manage().timeouts().implicitlyWait(60000,TimeUnit.SECONDS);
+			//driver.manage().timeouts().implicitlyWait(60000,TimeUnit.SECONDS);
 			Har har = server.getHar();
-			driver.manage().timeouts().implicitlyWait(60,TimeUnit.SECONDS);
+			//driver.manage().timeouts().implicitlyWait(60,TimeUnit.SECONDS);
 			FileOutputStream fos = new FileOutputStream("Sample.har");
 			har.writeTo(fos);
 		} catch (Exception e){
