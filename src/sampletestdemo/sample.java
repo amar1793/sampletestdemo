ab76b0eaeb251bd9ed97c2d54bf10c48bfcef962
Zuci Systems <Zuci Systems@192.168.1.6>
2016-11-02 11:30:53 +0530

    Updated.

--- src/sampletestdemo/sample.java
+++ src/sampletestdemo/sample.java
@@ -23,11 +23,10 @@ public class sample {
 		
 		try{
 			server.start();
-	
 			/*			Get the Selenium Proxy Object			*/
 			Proxy proxy = ClientUtil.createSeleniumProxy(server);
 		
-			capabilities = new DesiredCapabilities().android();
+		/*	capabilities = new DesiredCapabilities().android();
 			capabilities.setCapability(CapabilityType.BROWSER_NAME, "Browser");
 			capabilities.setCapability("automationName", "Appium");
 			capabilities.setCapability(CapabilityType.PLATFORM, "Android");
@@ -36,21 +35,37 @@ public class sample {
 			capabilities.setCapability("deviceName", "Zu");
 			capabilities.setCapability("appPackage", "com.android.browser");
 			capabilities.setCapability("appActivity", "com.android.browser.BrowserActivity");
-			capabilities.setCapability("noReset", false);
-		
+			capabilities.setCapability("noReset", false); */
 			
+			capabilities = new DesiredCapabilities().android();
+			capabilities.setCapability(CapabilityType.PROXY, proxy);
+			capabilities.setCapability("automationName", "Appium");
+			capabilities.setCapability("platformName", "Android");
+			capabilities.setCapability("platformVersion", "5.1.1");
+			capabilities.setCapability("deviceName", "emulator-5558");
+			capabilities.setCapability("browserName", "");
+			capabilities.setCapability("newCommandTimeout", 60);
+			capabilities.setCapability("appPackage", "org.mozilla.fennec");
+			capabilities.setCapability("appActivity", "org.mozilla.gecko.BrowserApp");
 		
-		server.newHar("http://www.facebook.com");
+			//driver = new RemoteWebDriver(capabilities);
+			//server.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
+			
+		server.newHar("http://www.rediff.com");
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		driver.get("http://www.facebook.com");
+		driver.manage().timeouts().implicitlyWait(60000, TimeUnit.SECONDS);
+		driver.get("http://www.rediff.com");
 		try{
+			driver.manage().timeouts().implicitlyWait(60000,TimeUnit.SECONDS);
 			Har har = server.getHar();
+			driver.manage().timeouts().implicitlyWait(60,TimeUnit.SECONDS);
 			FileOutputStream fos = new FileOutputStream("Sample.har");
 			har.writeTo(fos);
 		} catch (Exception e){
 			e.printStackTrace();
 		} 
+		} catch(Exception e){
+			e.printStackTrace();
 		} finally {
 			server.stop();
 			driver.close();
